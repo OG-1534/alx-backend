10726f1db3d89b010b5109c7c5cba167179f22e7
OG-1534 <gershomochanda@gmail.com>
2024-07-30 23:01:27 +0300

    setup a basic Flask app in 0-app.py. Create a single / route and an index.html template that simply outputs “Welcome to Holberton” as page title (<title>) and “Hello world” as header (<h1>)

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,16 +7,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     """
     Route for the hello_world page.
-    
+
     Returns:
     str: HTML rendered for the hello_world page.
     """
     return render_template('0-index.html')
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-
